dd2b8eb9216af0f76784a67484a7b7e060404d1c
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-19 17:31:32 +0100

    Hello world improved

--- FirstSolution/FirstProject/FirstProject.cpp
+++ FirstSolution/FirstProject/FirstProject.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello University of Information Technology and Management!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
