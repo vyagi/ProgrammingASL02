0dd1ebb7541a8879b3a3bb0c7e0a2adb72954978
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-06-11 15:57:16 +0200

    Cleaning up and initial work

---
+++ InvoiceManager/InvoiceManager/InvoiceManagerForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace InvoiceManager
+{
+    partial class InvoiceManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Location = new System.Drawing.Point(12, 38);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(75, 23);
+            this.readFileButton.TabIndex = 0;
+            this.readFileButton.Text = "Read";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(12, 15);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(29, 13);
+            this.pathLabel.TabIndex = 2;
+            this.pathLabel.Text = "Path";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(63, 12);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(725, 20);
+            this.pathTextBox.TabIndex = 3;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(13, 74);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(775, 364);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // InvoiceManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.readFileButton);
+            this.Name = "InvoiceManagerForm";
+            this.Text = "Invoice Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readFileButton;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
--- InvoiceManager/InvoiceManager/InvoiceManagerForm.cs
+++ InvoiceManager/InvoiceManager/InvoiceManagerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace InvoiceManager
 {
-    public partial class Form1 : Form
+    public partial class InvoiceManagerForm : Form
     {
-        public Form1()
+        public InvoiceManagerForm()
         {
             InitializeComponent();
         }
