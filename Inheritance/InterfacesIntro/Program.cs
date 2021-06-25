c96eea6451751eb43a4cb1af05af2c0ebd7fcfea
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-06-25 16:38:10 +0200

    Interfaces intro done

--- Inheritance/InterfacesIntro/Program.cs
+++ Inheritance/InterfacesIntro/Program.cs
@@ -2,13 +2,47 @@
 
 namespace InterfacesIntro
 {
-    
+    public interface IShape
+    {
+        double Area();
+    }
+
+    public interface ICreature
+    {
+        void Breathe();
+    }
+
+    public abstract class Shape : IShape, ICreature
+    {
+        public double Area()
+        {
+            return 0;
+        }
+
+        public void Breathe()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class LivingSquare : IShape, ICreature
+    {
+        public double Area() => 10;
+
+        public void Breathe()
+        {
+            Console.WriteLine("Breathing !!!");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-           
-
+            var ls = new LivingSquare();
+            Console.WriteLine(ls.Area());
+            ls.Breathe();
         }
     }
 }
